66e25f0f930269806d887cbf0452052c3762ff63
Pratham khatri <khatripratham349@gmail.com>
2024-09-23 04:50:44 +0530

    Refactor proxy server to handle multiple client connections

--- proxy-server-cache.c
+++ proxy-server-cache.c
@@ -37,12 +37,45 @@ pthread_t tid[MAX_CLIENTS];
 sem_t semaphore;
 pthread_mutex_t lock;
 
+int main(int argc, char *argv[])
+{
 
-int main(int argc, char * argv[]) {
+    int client_socketId, client_len;             // client_socketId == to store the client socket id
+    struct sockaddr_in server_addr, client_addr; // Address of client and server to be assigned
 
-	int client_socketId, client_len; // client_socketId == to store the client socket id
-	struct sockaddr_in server_addr, client_addr; // Address of client and server to be assigned
+    sem_init(&semaphore, 0, MAX_CLIENTS); // Initializing seamaphore and lock
+    pthread_mutex_init(&lock, NULL);      // Initializing lock for cache}
 
-    sem_init(&seamaphore,0,MAX_CLIENTS); // Initializing seamaphore and lock
-    pthread_mutex_init(&lock,NULL); // Initializing lock for cache}
+    // Infinite Loop for accepting connections
+    while (1)
+    {
+
+        bzero((char *)&client_addr, sizeof(client_addr)); // Clears struct client_addr
+        client_len = sizeof(client_addr);
+
+        // Accepting the connections
+        client_socketId = accept(proxy_socketId, (struct sockaddr *)&client_addr, (socklen_t *)&client_len); // Accepts connection
+        if (client_socketId < 0)
+        {
+            fprintf(stderr, "Error in Accepting connection !\n");
+            exit(1);
+        }
+        else
+        {
+            Connected_socketId[i] = client_socketId; // Storing accepted client into array
+        }
+
+        // Getting IP address and port number of client
+        struct sockaddr_in *client_pt = (struct sockaddr_in *)&client_addr;
+        struct in_addr ip_addr = client_pt->sin_addr;
+        char str[INET_ADDRSTRLEN]; // INET_ADDRSTRLEN: Default ip address size
+        inet_ntop(AF_INET, &ip_addr, str, INET_ADDRSTRLEN);
+        printf("Client is connected with port number: %d and ip address: %s \n", ntohs(client_addr.sin_port), str);
+        // printf("Socket values of index %d in main function is %d\n",i, client_socketId);
+        pthread_create(&tid[i], NULL, thread_fn, (void *)&Connected_socketId[i]); // Creating a thread for each client accepted
+        i++;
+    }
+    close(proxy_socketId); // Close socket
+    return 0;
 }
+
